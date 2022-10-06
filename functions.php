21e9e3dfed68a0c1b827b30b0b46a16ef83674a7
CorentinDelalle <corentindelalle@gmail.com>
2022-10-06 12:13:13 -0400

    modification de function pour affichage de menu sur wordpress admin

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * underscore functions and definitions
+ * underscores functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package underscore
+ * @package underscores
  */
 
 if ( ! defined( '_S_VERSION' ) ) {
@@ -12,17 +12,19 @@ if ( ! defined( '_S_VERSION' ) ) {
 	define( '_S_VERSION', '1.0.0' );
 }
 
-function underscore_setup() {
+function undercores_setup() {
+	
 
-  	/*
+    	/*
 		* Let WordPress manage the document title.
 		* By adding theme support, we declare that this theme does not use a
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
+    
 	add_theme_support( 'title-tag' );
 
-  	/*
+    	/*
 		* Switch default core markup for search form, comment form, and comments
 		* to output valid HTML5.
 		*/
@@ -38,18 +40,41 @@ function underscore_setup() {
 			'script',
 		)
 	);
+
+
+
 }
 
-add_action( 'after_setup_theme', 'underscore_setup' );
+add_action( 'after_setup_theme', 'undercores_setup' );
 
 /**
  * Enqueue scripts and styles.
  */
-function underscore_scripts() {
-  wp_enqueue_style('underscore-style',
-  get_template_directory_uri() . '/style.css',
-  array(),
-  filemtime(get_template_directory() . '/style.css'),false);
+function undercores_scripts() {
 
+	// wp_enqueue_style( 'undercores-style', 
+    //                    get_stylesheet_uri(), 
+    //                    array(), 
+    //                    _S_VERSION );
+
+    wp_enqueue_style('undercores-style',   // para poder agregar el archivo css dentro del tema
+                      get_template_directory_uri() . '/style.css', 
+                      array(), 
+                      filemtime(get_template_directory() . '/style.css'), 
+                      false);
+	
 }
-add_action( 'wp_enqueue_scripts', 'underscore_scripts' );
+add_action( 'wp_enqueue_scripts', 'undercores_scripts' );
+
+
+
+
+/** ---------------------------------------------------------- */
+
+function mon_31w_register_nav_menu(){
+	register_nav_menus( array(
+		'menu_primaire' => __( 'Menu Primaire', 'text_domain' ),
+	
+	) );
+}
+add_action( 'after_setup_theme', 'mon_31w_register_nav_menu', 0 );
