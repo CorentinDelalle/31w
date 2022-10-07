e463b526ac3601c26bd5f4394198f5183ae01832
CorentinDelalle <corentindelalle@gmail.com>
2022-10-07 14:08:45 -0400

    modification des styles articles

--- front-page.php
+++ front-page.php
@@ -22,11 +22,12 @@
             while ( have_posts() ) :
 				the_post(); // puede ser un post o un articulo 
                 // the_title('<h2> ','</h2>');?>
+                <div class="articleBlog">
                 <h1><a href=" <?php the_permalink()?> "><?php the_title() ?></a></h1>
                 <?php the_content(null,true);
+                ?></div>
                
-                ; // quienes son los titulos y los post, nos muestras el link del post 
-            endwhile;
+           <?php endwhile;
         endif;        
     ?>            
     </main>
